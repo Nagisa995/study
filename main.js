aed72259a84449db6ffeedf79e534960bdfdb6d6
Nagisa <miku195@mail.ru>
2022-01-26 00:17:42 +0700

    function with calculator through object

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ function Calc(action,number1,number2)
 {
     const operations=
     {
-        sum:number1+number2,
+        sum:number1+(+number2),
         sub:number1-number2,
         mult:number1*number2,
         div:number1/number2,
     };
-    return (operations[action]);
+    if((action in operations)&&(+number1||number1=='0')&&(+number2||number2=='0')) return(operations[action]);
+    else    return (action in operations? 'Error':'unknown operation');    
 }
 
 console.log(Calc('sub',600,30));
-console.log(Calc('sum',600,30));
+console.log(Calc('sum',600,'30'));
 console.log(Calc('mult',6,3));
 console.log(Calc('div',60,3));
+console.log(Calc('awdqv',6,3));
+console.log(Calc('mult',6,''));
