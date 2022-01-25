921bd082da479876f9cd357b2356faa5bcc44533
Nagisa <miku195@mail.ru>
2022-01-25 22:39:15 +0700

    function with calculator through object

--- main.js
+++ main.js
@@ -1,65 +1,16 @@
-function failnumber(a)
+function Calc(action,number1,number2)
 {
-if (+a||a=='0')
-return (+a);
-else    
-{
-    alert ('Error');
-    return (a='false');
-}
-}
-function sum(a,b)
-{return (a+b)}
-function differ(a,b)
-{return (a-b)}
-function multi(a,b)
-{return (a*b)}
-function division(a,b)
-{return (a/b)}
-function degree(a,b)
-{return (a**b)}
-{
-let numb1,operator, numb2;
-do
-{
-numb1=failnumber(numb1=prompt('Введите первое число'));
-if (numb1=='false') continue;
-operator=prompt('Какую операцию хотите выполнить +,-,*,/,**(степень)');
-switch (operator)
-{
-case '+':
-    numb2=failnumber(numb2=prompt('Введите второе слагаемое'));
-    if (numb2=='false') continue;
-    alert ('Результат сложения '+sum(numb1,numb2)); 
-    break;
-case '-':
-    numb2=failnumber(numb2=prompt('Введите вычетаемое значение'));
-    if (numb2=='false') continue;
-    alert ('Результат вычетания '+differ(numb1,numb2)); 
-    break;
-case '*':
-    numb2=failnumber(numb2=prompt('Введите второй множитель'));
-    if (numb2=='false') continue;
-    alert ('Результат умножения '+multi(numb1,numb2)); 
-    break;
-case '/':
-    numb2=failnumber(numb2=prompt('Введите делитель'));
-    if (numb2=='false') continue;
-    if (numb2=='0') 
+    const operations=
     {
-        alert ('На ноль делить нельзя');
-        continue;
-    }
-    alert ('Результат деления '+division(numb1,numb2)); 
-    break;
-case '**':
-    numb2=failnumber(numb2=prompt('Введите степень в которую возводим'));
-    if (numb2=='false') continue;
-    alert ('Результат возведения в степень '+degree(numb1,numb2)); 
-    break;
-default:
-    alert('Unknown operation');       
+        sum:number1+number2,
+        sub:number1-number2,
+        mult:number1*number2,
+        div:number1/number2,
+    };
+    return (operations[action]);
 }
-}while(confirm('Повторный рассчет?'));
-alert('Всего доброго!');
-}
+
+console.log(Calc('sub',600,30));
+console.log(Calc('sum',600,30));
+console.log(Calc('mult',6,3));
+console.log(Calc('div',60,3));
