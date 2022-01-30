f266b8186c6a85d04c4b4969fe4542bdcbc21ad1
Nagisa <miku195@mail.ru>
2022-01-30 17:26:05 +0700

    v1.2

--- todo.js
+++ todo.js
@@ -1,30 +1,33 @@
-{
-    const list = { 'create a new practice task': 'In Progress', 'make a bad': 'Done', 'write a post': 'To Do', };
-    const status = { 'To Do': 'Todo:', 'In Progress': 'In Progress:', Done: 'Done:', };
-    function changeStatus(task, status) {
-        return (list[task] = status);
-    }
-    function addTask(task) {
-        return (list[task] = 'To Do');
-    }
-    function deleteTask(task) {
-        return (delete list[task]);
-    }
-    function showList(list) {
-        for (let write in status) {
-            console.log(status[write]);
-            let count = 0;
-            for (let task in list) {
-                if (list[task] == write) {
-                    console.log('"' + task + '"');
-                    count++;
-                }
-            }
-            if (count == 0) console.log('-');
-        }
-    }
-    changeStatus('create a new practice task', 'To Do');
-    changeStatus('make a bad', 'To Do');
-    changeStatus('write a post', 'In Progress');
-    showList(list);
+const STATUS = { 'To Do': 'Todo:', 'In Progress': 'In Progress:', Done: 'Done:', };
+const list = { 'create a new practice task': 'In Progress', 'make a bad': 'Done', 'write a post': 'To Do', };
+function changeStatus(task, status) {
+    return (list[task] = status);
 }
+function addTask(task) {
+    return (list[task] = 'To Do');
+}
+function deleteTask(task) {
+    return (delete list[task]);
+}
+function showList() {
+    for (let write in STATUS) {
+        let onScreen = STATUS[write];
+        for (let task in list) {
+            if (list[task] == write) {
+                onScreen += '\n' + task;
+            }
+        }
+        (onScreen == STATUS[write]) ? console.log(onScreen + '\n-') : console.log(onScreen);
+    }
+}
+
+
+
+
+
+changeStatus('create a new practice task', 'To Do');
+changeStatus('make a bad', 'To Do');
+changeStatus('write a post', 'In Progress');
+showList();
+addTask('dominate world');
+showList();
